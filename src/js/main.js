dba7cb68012161592c8269af77bba291c0124151
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-11-05 19:41:59 +0300

    connected jquery and slick using npm and did the second part of the task

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
-import showModal from './modules/modal';
+import './services/slider';
 
-showModal();
+import modal from './modules/modal';
+
+modal();
--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,21 +1,35 @@
 'use strict';
 
-function showModal() {
- const popupEngineer = document.querySelector('.popup_engineer');
- const popupEngineerBtn = document.querySelector('.popup_engineer_btn');
+function modal() {
+    const modalEngineer = document.querySelector('.popup_engineer');
+    const modalEngineerOpenBtn = document.querySelector('.popup_engineer_btn');
+    const modalCall = document.querySelector('.popup');
+    const modalCallOpensBtn = document.querySelectorAll('.phone_link');
 
-
- popupEngineerBtn.addEventListener('click', (e) => {
-     popupEngineer.classList.add('show');
- });
-
-
- popupEngineer.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target === popupEngineer || target.parentElement.classList.contains('popup_close')) {
-        popupEngineer.classList.remove('show');
+    function modalOpen(modal, modalOpenBtn) {
+        modalOpenBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.classList.add('show');
+        });
     }
- });
+
+    modalOpen(modalEngineer, modalEngineerOpenBtn);
+
+    modalCallOpensBtn.forEach(modalCallOpenBtn => {
+        modalOpen(modalCall, modalCallOpenBtn);
+    })
+
+    function modalClose(modal) {
+        modal.addEventListener('click', (e) => {
+            const target = e.target;
+            if (target === modal || target.parentElement.classList.contains('popup_close')) {
+                modal.classList.remove('show');
+            }
+        });
+    }
+
+    modalClose(modalEngineer);
+    modalClose(modalCall);
 }
 
-export default showModal;
+export default modal;
